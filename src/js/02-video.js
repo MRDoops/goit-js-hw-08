3b2bc36826f898f5b0554399e1e5346110a340b8
Dmytro Martynenko <DoopsDM@gmail.com>
2023-03-04 13:54:25 +0200

    added a listener to the player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const player = new Player('vimeo-player', {
+  id: 236203659,
+  width: 640,
+  height: 360,
+  autopause: false,
+});
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 1000)
+);
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
